04f27055cd6d62493c5277a851a9bf6295b8475c
gbaste <gbfabre@gmail.com>
2018-06-09 12:17:54 +0200

    add hundir la flota js skylab

---
+++ app.js
@@ -0,0 +1,39 @@
+function hundir(){
+
+    var arrayFlota = []
+    
+    
+    function userName(){ 
+        var person = prompt("Introduzca su nombre"); 
+          if (person) { 
+            return person;
+          } else { (person != typeof(string)) 
+            return userName();
+          } 
+      }
+
+
+function randomNum(){
+
+    var number = Math.floor((Math.random() * 2) + 1)
+    return number
+
+}
+
+function generateMap(){
+
+    var arr = new Array(5).fill('🌊')
+    console.log(arr)
+
+}
+
+
+
+var person = username()
+console.log(person)
+generate()
+
+
+}
+
+hundir()
